6b649a72b98d6c47e14f2b418532a228341d9308
Ian <blackhydrogen@users.noreply.github.com>
2016-02-28 02:13:11 +0800

    Renamed .sh and /hooked now runs shell script.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var exec = require("child_process").exec;
+
 var app = express();
 var server = http.Server(app);
 var httpPort = 23444;
@@ -10,7 +12,23 @@ var httpPort = 23444;
 app.use(bodyParser.json());
 
 app.post("/hooked", function(req, res) {
+	console.log("==================== HOOKED ====================")
 	console.log(req.body);
+	
+	console.log("Executing shell script...");
+	
+	exec("./onhook_run.sh" + req.params.movie,
+	function (error, stdout, stderr) {
+		console.log("stdout: " + stdout);
+		console.log("stderr: " + stderr);
+		if (error !== null)
+			console.log("exec error: " + error);
+		
+		console.log("Shell script done.");
+		console.log();
+		console.log();
+	});
+	
 	res.end("OK");
 });
 
