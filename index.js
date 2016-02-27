09b6162f069fe5726a38078ebb0e05e4069da423
Ian <blackhydrogen@users.noreply.github.com>
2016-02-28 01:21:57 +0800

    Basic hook set up.

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var server = http.Server(app);
+var httpPort = 23444;
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/hooked", function(req, res) {
+	console.log(req.body);
+	res.end("OK");
+});
+
+server.listen(httpPort, function() {
+	console.log("Listening on *:" + httpPort);
+});
